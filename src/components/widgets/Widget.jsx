4fe0dad1a3f4279e7f0f9962fd6439cd33022002
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-01-16 15:12:22 +0500

    widget part completed

--- src/components/widgets/Widget.jsx
+++ src/components/widgets/Widget.jsx
@@ -1,9 +1,68 @@
+import Keyboard from "@mui/icons-material/KeyboardArrowUp";
+import Person from "@mui/icons-material/PersonOutlined";
+import Shopping from "@mui/icons-material/ShoppingCartOutlined";
+import Monetization from "@mui/icons-material/MonetizationOnOutlined";
+import Balance from "@mui/icons-material/AccountBalanceOutlined";
 import "./Widget.scss";
-const Widget = () => {
+const Widget = ({ type }) => {
+	let data;
+
+	const amount = 100;
+	const diff = 33;
+
+	switch (type) {
+		case "user":
+			data = {
+				title: "USERS",
+				isMoney: false,
+				link: "See all users",
+				icon: <Person className="widget__icon widget__icon--person" />,
+			};
+			break;
+		case "order":
+			data = {
+				title: "ORDERS",
+				isMoney: false,
+				link: "View all orders",
+				icon: <Shopping className="widget__icon widget__icon--shopping" />,
+			};
+			break;
+		case "earning":
+			data = {
+				title: "EARNINGS",
+				isMoney: true,
+				link: "View net earnings",
+				icon: <Monetization className="widget__icon widget__icon--money" />,
+			};
+			break;
+		case "balance":
+			data = {
+				title: "BALANCE",
+				isMoney: true,
+				link: "See all details",
+				icon: <Balance className="widget__icon widget__icon--balance" />,
+			};
+			break;
+		default:
+			break;
+	}
+
 	return (
 		<div className="widget">
-			<div className="widget__left"></div>
-			<div className="widget__right"></div>
+			<div className="widget__left">
+				<span className="widget__title">{data.title}</span>
+				<span className="widget__counter">
+					{data.isMoney && "$"} {amount}
+				</span>
+				<span className="widget__link">{data.link}</span>
+			</div>
+			<div className="widget__right">
+				<div className="widget__percentage widget__positive">
+					<Keyboard />
+					{diff}%
+				</div>
+				{data.icon}
+			</div>
 		</div>
 	);
 };
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,6 +1,17 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import DataTable from "../../components/datatable/Datatable.jsx";
 import "./List.scss";
 const List = () => {
-	return <div>List</div>;
+	return (
+		<div className="list">
+			<Sidebar />
+			<div className="list__container">
+				<Navbar />
+				<DataTable />
+			</div>
+		</div>
+	);
 };
 
 export default List;
